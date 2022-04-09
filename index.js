b88b9dcab90de7314a25ba3a5c908524f9d270fe
siddhirajpantoji <siddhirajpantoji@gmail.com>
2022-04-09 13:46:33 +0530

    Changes for Node modules

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const express=require('express');
-const app=express();
-const http=require('http');
-const { disconnect } = require('process');
-const server=http.createServer(app);
-const {Server}=require("socket.io")
-const io=new Server(server)
+const express = require('express');
+const moment = require('moment-timezone')
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io")
+const io = new Server(server)
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-console.log('a user connected');
-socket.on('disconnect',() => {
-  console.log('user disconnected');
-});
-});
-
-io.on('connection', (socket) => {
-    socket.on('chat message',(msg) => {
-        io.emit('chat message',msg);
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('chat-message-recieve', (msg) => {
+        console.log("Chat Message recieve Event ")
+        msg.timestamp = moment().format("YYYY-MM-DD HH:mm:ss")
+        io.emit('chat-message-send', msg);
+        console.log("Sending message to client ")
         console.log('message: ' + msg);
     });
 });
 
-server.listen(3000,() => {
+server.listen(3000, () => {
     console.log('Listening on *: 3000');
 });
